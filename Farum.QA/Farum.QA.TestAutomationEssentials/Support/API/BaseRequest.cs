f51b07c6ec6f74bb97e0a1e23f1d68330f6233dc
pniedzialek <pniedzialek@altimetrik.com>
2022-04-27 10:45:43 +0200

    Added Restsharp and BaseRequest; added Browser Enums

---
+++ Farum.QA/Farum.QA.TestAutomationEssentials/Support/API/BaseRequest.cs
@@ -0,0 +1,32 @@
+﻿using RestSharp;
+
+namespace Farum.QA.TestAutomationEssentials.Support.API
+{
+    /// <summary>
+    /// Class intended for creation of Requests. 
+    /// </summary>
+    public class BaseRequest
+    {
+        protected RestClient client;
+        protected RestRequest request;
+
+        protected BaseRequest(string url, Method httpMethod)
+        {
+            client = new RestClient(url);
+            client.Timeout = -1;
+            request = new RestRequest(httpMethod);
+            request.AddHeader("authorization", "SOMEAUTHORIZATION");
+            request.AddHeader("Content-Type", "application/json");
+        }
+
+        protected void SetPayload(string json)
+        {
+            request.AddParameter("application/json", json, ParameterType.RequestBody);
+        }
+
+        protected IRestResponse SendRequest()
+        {
+            return client.Execute(request);
+        }
+    }
+}
---
+++ Farum.QA/Farum.QA.TestAutomationEssentials/Support/Enums/BrowserTypes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Farum.QA.TestAutomationEssentials.Support.Enums
+{
+    public enum BrowserTypes
+    {
+        InternetExplorer,
+        Chrome,
+        SauceLabs,
+        Firefox
+    }
+}
