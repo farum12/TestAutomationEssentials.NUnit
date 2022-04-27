bd2c9309eccabc0bf605083fd69057054d70f973
pniedzialek <pniedzialek@altimetrik.com>
2022-04-27 10:54:44 +0200

    Added RandomGenerator and Wait

---
+++ Farum.QA/Farum.QA.TestAutomationEssentials/Support/RandomGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace Farum.QA.TestAutomationEssentials.Support
+{
+    public class RandomGenerator
+    {
+        public static string GetRandomString(int length)
+        {
+            Random random = new Random();
+            const string chars = "ABCDEFGHIJKLMNOPRSTUWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        public static string GetGuid()
+        {
+            return $"{GetRandomString(8)}-{GetRandomString(4)}-{GetRandomString(4)}-{GetRandomString(4)}-{GetRandomString(12)}";
+        }
+    }
+}
---
+++ Farum.QA/Farum.QA.TestAutomationEssentials/Support/Wait.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+
+namespace Farum.QA.TestAutomationEssentials.Support
+{
+    /// <summary>
+    /// Class intended to contain more complex waiting methods.
+    /// </summary>
+    public static class Wait
+    {
+        /// <summary>
+        /// Mimics Wait.Until; executes certain task (eg. method) within given timeout and pauses if task has returned false; retries until task returns true or timeout time is exceeded.
+        /// DO NOT USE WITH WEBDRIVER. Use in API calls.
+        /// </summary>
+        /// <param name="task">Task to be executed and retried until it succeeds. Example value: () => SomeMethod(arg1,arg2)</param>
+        /// <param name="timeout">Time till method tries to execute given task.</param>
+        /// <param name="pause">Time for which thread pauses and retries the task.</param>
+        public static bool UntilSuccessOrTimeout(Func<bool> task, TimeSpan timeout, TimeSpan pause)
+        {
+            if (pause.TotalMilliseconds < 0)
+            {
+                throw new ArgumentException("pause must be >= 0 miliseconds");
+            }
+            var stopwatch = Stopwatch.StartNew();
+            do
+            {
+                if (task()) { return true; }
+                Thread.Sleep((int)pause.TotalMilliseconds);
+            } while (stopwatch.Elapsed < timeout);
+            return false;
+        }
+
+        public static void UntilPageHasChangedDuringAction(Drivers.WebDriver webDriver, Action task)
+        {
+            var currentPageTitle = webDriver.Current.Title;
+            task();
+            webDriver
+                .Wait("Page did not change in expected time!", TimeSpan.FromSeconds(12))
+                .Until(ExpectedConditions.PageTitleChangedFrom(currentPageTitle));
+        }
+    }
+}
